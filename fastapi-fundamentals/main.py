454fe13fc6184c55e334400573f6f3c287574bb6
ravitejasatyavada <raviteja.satyavada@gmail.com>
2026-03-08 00:48:55 +0530

    FastAPI Fundamentals - Subjective assignment

---
+++ fastapi-fundamentals/main.py
@@ -0,0 +1,21 @@
+"""
+Query Parameters
+Requirement:
+Create a GET endpoint /search that accepts two optional query parameters:
+
+name (string)
+age (integer)
+Return the received parameters as JSON.
+"""
+
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+@app.get("/search")
+def search_method(name: str = Query(default="raviteja", description="Name parameter of the input"),
+                  age: int = Query(default=36, description="Age parameter of the input")
+                  ):
+    return {"name": name, "age": age}
+
