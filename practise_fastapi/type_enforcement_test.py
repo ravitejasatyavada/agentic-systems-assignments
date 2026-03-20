53f8e4fb796b6f31658a1be1216ff519d749b74a
ravitejasatyavada <raviteja.satyavada@gmail.com>
2026-03-20 20:57:05 +0530

    added the exception towards end of the file for Deepak's review

--- practise_fastapi/type_enforcement_test.py
+++ practise_fastapi/type_enforcement_test.py
@@ -33,3 +33,16 @@ print(student.id)
 students = {"id": 100, "name": "Raviteja", "parents_contact": {"father": 12345, "mother": 67890}}
 student = TypeEnforcementTest(**students)
 print(student.parents_contact)
+
+"""
+Exception throwing -- 
+Traceback (most recent call last):
+  File "/Users/tejasahiti/PycharmProjects/agentic-systems-assignments/practise_fastapi/type_enforcement_test.py", line 21, in <module>
+    student = TypeEnforcementTest(**students)
+  File "/Library/Frameworks/Python.framework/Versions/3.8/lib/python3.8/site-packages/pydantic/main.py", line 214, in __init__
+    validated_self = self.__pydantic_validator__.validate_python(data, self_instance=self)
+pydantic_core._pydantic_core.ValidationError: 1 validation error for TypeEnforcementTest
+parents_contact
+  Field required [type=missing, input_value={'id': 100, 'name': 'ravi'}, input_type=dict]
+    For further information visit https://errors.pydantic.dev/2.10/v/missing
+"""
